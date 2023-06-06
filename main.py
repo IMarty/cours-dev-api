922d56ef026ebc0e48704786cd4a8caeae4fb904
IMarty <igor.marty@telecomnancy.net>
2023-06-06 11:54:34 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,23 @@
+import http.server # Parametrage : location, handler 
+import socketserver # Ecoute
+
+#Handler
+class APIHandler(http.server.SimpleHTTPRequestHandler):
+    def do_GET(self) :
+        print("Coucou")
+        self.send_response(200)
+        self.send_header('Content-type','text/plain')
+        self.end_headers()
+        self.wfile.write("coucou".encode('utf-8'))
+
+MyAPIHandler = APIHandler
+
+# Server
+try:
+    with socketserver.TCPServer(("",8080), MyAPIHandler) as httpd:
+        print("server working")
+        httpd.allow_reuse_address = True
+        httpd.serve_forever()
+except KeyboardInterrupt:
+    print("Stopping server")
+    httpd.server_close()
