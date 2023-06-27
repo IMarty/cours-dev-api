28627a989b83a26cd94cd12f33e638447244cfa1
IMarty <igor.marty@telecomnancy.net>
2023-06-27 14:58:48 +0200

    perfect refractor

--- main.py
+++ main.py
@@ -1,31 +1,17 @@
 
-from fastapi import Depends, FastAPI
-from sqlalchemy.orm import Session 
+from fastapi import FastAPI
 
-import models # Import des ORM
-from database import get_cusor, database_engine # import du cursor/session de la DB
+import models_orm # Import des ORM
+from database import database_engine # import du cursor/session de la DB
 
+#Import des routers
+import router_products
 
 # Créer les tables si elles ne sont pas présente dans la DB
-models.Base.metadata.create_all(bind=database_engine)
+models_orm.Base.metadata.create_all(bind=database_engine)
 
+#Lancement de l'API
 app= FastAPI()
 
-
-# Read
-@app.get('/products')
-async def get_products(cursor: Session= Depends(get_cusor)):
-    print(cursor.query(models.Products)) # Requète SQL générée
-    all_products = cursor.query(models.Products).all() # Lancement de la requête
-    return {
-        "products": all_products,
-        "limit": 10,
-        "total": 2,
-        "skip":0
-    }
-
-# Exercice :  @app.get('/products/{product_id}')
-# db.query(models.BlogPosts).filter(models.BlogPosts.id == blog_id).first()
-# Connecter à votre propre Database URL
-
-# CREATE / DELETE / UPDATE
+# Ajouter les routers dédiés
+app.include_router(router_products.router)
